9f326c7a421dc12ba9f741087ee1bfa8d33bdfc9
baptistejouin <baptiste.jouin44@gmail.com>
2024-03-05 15:23:25 +0100

    feat: support refresh token

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -2,14 +2,41 @@ export default defineNuxtRouteMiddleware((to, from) => {
 
 	const access_token = useCookie('accessToken');
 
-	if (to.path === '/login' && access_token.value) {
-		return navigateTo('/')
+	// is auth
+	if (access_token.value) {
+
+		if (to.path === '/login') {
+			return navigateTo('/')
+		}
+
+		// next auth route
+		return;
 	}
 
-	// it is important to check `to.path` before redirecting or you might get an infinite redirect loop
-	if (!access_token.value && to.path !== '/login') {
-		return navigateTo('/login')
+	// no auth
+	if (!access_token.value) {
+
+		// refresh token ?
+		const refresh_token = useCookie('refreshToken');
+
+		if (refresh_token.value) {
+			// refresh token
+			return navigateTo({
+				path: '/api/auth/refresh',
+				query: {
+					refresh_token: refresh_token.value
+				},
+				replace: true
+			})
+		}
+
+		// it is important to check `to.path` before redirecting or you might get an infinite redirect loop
+		if (to.path !== '/login') {
+			return navigateTo('/login')
+		}
+
+		// next no auth route
+		return;
 	}
 
-	return;
 })
--- server/api/auth/refresh.get.ts
+++ server/api/auth/refresh.get.ts
@@ -2,20 +2,20 @@ export default defineEventHandler(async (event) => {
 
 	const config = useRuntimeConfig()
 
-	const code = getQuery(event).code?.toLocaleString()
+	const refresh_token = getQuery(event).refresh_token?.toLocaleString()
 
-	if (!code) {
+	if (!refresh_token) {
 		return {
-			error: "Required query parameter 'code' is missing.",
-			event: code
+			error: "Required query parameter 'refresh_token' is missing.",
+			event: refresh_token
 		}
 	}
 
 	const params = new URLSearchParams();
 	params.append('client_id', config.public.apiClientID);
 	params.append('client_secret', config.apiClientSecret);
-	params.append('code', code);
-	params.append('grant_type', 'authorization_code');
+	params.append('grant_type', 'refresh_token');
+	params.append('refresh_token', refresh_token);
 
 	const options = {
 		method: 'POST',
@@ -28,6 +28,14 @@ export default defineEventHandler(async (event) => {
 	const data = await fetch('https://www.strava.com/oauth/token', options).then(res => res.json())
 
 	if (data.errors) {
+		// force remove refreshToken
+		setCookie(event, "refreshToken", '', {
+			httpOnly: false,
+			secure: true,
+			sameSite: 'strict',
+			maxAge: 0
+		})
+
 		return {
 			data
 		}
@@ -38,13 +46,6 @@ export default defineEventHandler(async (event) => {
 		secure: true,
 		sameSite: 'strict',
 		maxAge: data.expires_in
-	})
-
-	setCookie(event, "refreshToken", data.refresh_token, {
-		httpOnly: false,
-		secure: true,
-		sameSite: 'strict',
-		maxAge: 2592000 // 30 days
 	})
 
 	await sendRedirect(event, '/', 200)
